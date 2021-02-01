a5d44f2b6f5801b763452296cf965167241cb4f9
RasmusBC59 <rbcmail@gmail.com>
2021-02-01 19:01:17 +0100

    added back_of_beans

---
+++ back_of_beans.py
@@ -0,0 +1,67 @@
+import qcodes as qc
+from qcodes.instrument.base import Instrument, Parameter
+import broadbean as bb
+from broadbean.plotting import plotter
+import numpy as np
+from typing import Dict, Callable, List, Optional, Sequence
+
+class BagOfBeans(Instrument):
+    """
+    Class that turns a broadbean Sequence into a QCoDeS Intrument 
+
+        Parameters
+        ----------
+        seq (Parseq): Broadbean Sequence tured into a QCoDeS Parameter
+        seq_path (str): Path to sequence file
+        SR (float): sampling rate          
+    """
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+        self.add_parameter(name='seq',
+                           label='Sequence',
+                           parameter_class=ParSeq)        
+        self.add_parameter(name='seq_path',
+                           label='Path to sequence file',
+                           get_cmd=None,
+                           set_cmd=None) 
+        self.add_parameter(name='SR',
+                            label='sample rate',
+                            unit='Hz',
+                            parameter_class=sample_rate,
+                            snapshot_exclude = True)
+
+class ParSeq(Parameter):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.seq = bb.Sequence() 
+    
+    def snapshot_base(
+        self, update: bool = False,
+        params_to_skip_update: Optional[Sequence[str]] = None) -> Dict:
+        return self.seq.description #{'status': self.status}
+    
+    def get_raw(self):
+        return self.seq
+
+    def set_raw(self,newseq):
+        self.seq = newseq
+
+    def from_file(self):
+        seq_path = self.root_instrument.seq_path()
+        self.seq = bb.Sequence.init_from_json(seq_path)
+    
+    def to_file(self):
+        seq_path = self.root_instrument.seq_path()
+        self.seq.write_to_json(seq_path)      
+
+
+class sample_rate(Parameter):
+    
+    def get_raw(self):
+        seq = self.root_instrument.seq()
+        return seq.SR
+    
+    def set_raw(self,SR):
+        seq = self.root_instrument.seq()
+        seq.setSR(SR)
+
